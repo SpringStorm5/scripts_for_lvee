4baeeabaa380251a72ac0883f58d071c085462e9
SpringStorm5 <springstorm666@gmail.com>
2016-10-28 00:09:18 +0300

    test commit

--- lveestart.sh
+++ lveestart.sh
@@ -9,7 +9,7 @@ then sudo dnf install  libxml2-devel libmysqlclient-devel git curl; fi
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable
 source ~/.rvm/scripts/rvm
-pwd
+#pwd
 git clone https://github.com/lvee/lvee-engine
 cd lvee-engine/
 #git checkout staging
