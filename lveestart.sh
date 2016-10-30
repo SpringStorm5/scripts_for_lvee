5edce057de0265266c07f8fdb1bd0ed96e78a3dd
SpringStorm5 <springstorm666@gmail.com>
2016-10-30 13:44:37 +0300

    add variable for mysql password

--- lveestart.sh
+++ lveestart.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
+  if [[ -n "$1" ]]
+    then parol=$1
+  else
+    parol="default"
+  fi
 Ubuntu=$(uname -a | grep -oi Ubuntu)
 Fedora=$(uname -a | grep -oi Fedora)
 if [[ $Ubuntu = "Ubuntu" ]]
 then sudo apt-get install git curl libmysqlclient-dev libpq-dev libev-dev; fi
 if [[ $Fedora = "fedora" ]]
 then sudo dnf install  libxml2-devel libmysqlclient-devel git curl; fi
-
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable
 source ~/.rvm/scripts/rvm
 #pwd
 git clone https://github.com/lvee/lvee-engine
 cd lvee-engine/
-#git checkout staging
+git checkout staging
 rvm install $(cat .ruby-version) #может быть долго
 rvm use $(cat .ruby-version)
 gem install bundler
 bundle install
 touch config/databasemy.yml
-cat config/database.yml | sed  's/P@ssw0rd/12345/g' > config/databasemy.yml
+cat config/database.yml | sed  "s/P@ssw0rd/$parol/g" > config/databasemy.yml
 mv config/databasemy.yml config/database.yml
 service mysql start
 branch=$(git rev-parse --abbrev-ref HEAD)
