d4d14016d8979217674c7c7b359247240bc97812
Alexandr D <alex-liberty@users.noreply.github.com>
2016-11-04 23:10:41 +0300

    Fix dependences (#4)

--- lveestart.sh
+++ lveestart.sh
@@ -43,9 +43,9 @@ done
 Ubuntu=$(uname -a | grep -oi Ubuntu)
 Fedora=$(uname -a | grep -oi Fedora)
 if [[ $Ubuntu = "Ubuntu" ]]
-then sudo apt-get install git curl libmysqlclient-dev libpq-dev libev-dev; fi
+then sudo apt-get install git curl nodejs libmysqlclient-dev libpq-dev libev-dev; fi
 if [[ $Fedora = "fedora" ]]
-then sudo dnf install  libxml2-devel libmysqlclient-devel git curl; fi
+then sudo dnf install  git curl nodejs mariadb mariadb-server; fi
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable
 source ~/.rvm/scripts/rvm
@@ -59,7 +59,7 @@ gem install bundler
 bundle install
 sed -i "/config.i18n.backend = I18nDatabaseBackend.new/i \  config.action_mailer.raise_delivery_errors = false\n  config.action_mailer.delivery_method = :smtp\n  config.action_mailer.smtp_settings = {\n    :address => 'smtp.gmail.com',\n    :port => '587',\n    :user_name => '$email',\n    :password => '$email_pass',\n    :authentication => 'plain',\n    :enable_starttls_auto => true }\n" ./config/environments/development.rb
 sed -i "s/P@ssw0rd/$parol/g" config/database.yml
-sudo service mysql start
+sudo service mysql start # sudo service mariadb start
 branch=$(git rev-parse --abbrev-ref HEAD)
 if [[ $branch = "staging" ]]; then bundle exec rails bootstrap; fi
 if [[ $branch = "master" ]]; then bundle exec rake bootstrap; fi
@@ -67,3 +67,4 @@ sed -i "s/lvee.org/localhost:3000/g" config/initializers/constants.rb
 sed -i "s/https/http/g" config/initializers/constants.rb
 bundle exec rails s
 bundle exec rake db:drop:all
+
