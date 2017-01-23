338786dfa3aeac7c3686cced915172d4f985b888
SpringStorm5 <Springstorm666@gmail.com>
2017-01-23 13:03:53 +0300

    add start service mariadb in Fedora and commented db:drop:all

--- lveestart.sh
+++ lveestart.sh
@@ -63,7 +63,9 @@ gem install bundler
 bundle install
 sed -i "/config.i18n.backend = I18nDatabaseBackend.new/i \  config.action_mailer.raise_delivery_errors = false\n  config.action_mailer.delivery_method = :smtp\n  config.action_mailer.smtp_settings = {\n    :address => 'smtp.gmail.com',\n    :port => '587',\n    :user_name => '$email',\n    :password => '$email_pass',\n    :authentication => 'plain',\n    :enable_starttls_auto => true }\n" $fold/config/environments/development.rb
 sed -i "s/P@ssw0rd/$parol/g" $fold/config/database.yml
-sudo service mysql start # sudo service mariadb start
+sudo service mysql start
+if [[ $Fedora = "fedora" ]]
+then sudo service mariadb start; fi
 cd $fold
 "bin/rails db:environment:set RAILS_ENV=development"
 branch=$(git rev-parse --abbrev-ref HEAD)
@@ -72,5 +74,5 @@ if [[ $branch = "master" ]]; then bundle exec rake bootstrap; fi
 sed -i "s/lvee.org/localhost:3000/g" $fold/config/initializers/constants.rb
 sed -i "s/https/http/g" $fold/config/initializers/constants.rb
 bundle exec rails s
-bundle exec rake db:drop:all
+#bundle exec rake db:drop:all
 
