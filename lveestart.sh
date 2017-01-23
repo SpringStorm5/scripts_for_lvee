ed453b291d5864bb81c0f3763fb8b5c0c2535b2e
SpringStorm5 <Springstorm666@gmail.com>
2017-01-23 12:01:28 +0300

    add parametr folder in git clone and add RAILS_ENV

--- lveestart.sh
+++ lveestart.sh
@@ -6,6 +6,11 @@ do
 
     case $opt in
 
+    --folder)
+      fold="$1"
+      shift
+      ;;
+
     --pass)
       parol="$1"
       shift
@@ -40,31 +45,32 @@ do
 
 done
 
-Ubuntu=$(uname -a | grep -oi Ubuntu)
-Fedora=$(uname -a | grep -oi Fedora)
-if [[ $Ubuntu = "Ubuntu" ]]
-then sudo apt-get install git curl nodejs libmysqlclient-dev libpq-dev libev-dev; fi
-if [[ $Fedora = "fedora" ]]
-then sudo dnf install  git curl nodejs mariadb mariadb-server; fi
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-curl -sSL https://get.rvm.io | bash -s stable
-source ~/.rvm/scripts/rvm
-#pwd
-git clone https://github.com/lvee/lvee-engine
-cd lvee-engine
+#Ubuntu=$(uname -a | grep -oi Ubuntu)
+#Fedora=$(uname -a | grep -oi Fedora)
+#if [[ $Ubuntu = "Ubuntu" ]]
+#then sudo apt-get install git curl nodejs libmysqlclient-dev libpq-dev libev-dev; fi
+#if [[ $Fedora = "fedora" ]]
+#then sudo dnf install  git curl nodejs mariadb mariadb-server; fi
+#gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+#curl -sSL https://get.rvm.io | bash -s stable
+#source ~/.rvm/scripts/rvm
+git clone https://github.com/lvee/lvee-engine $fold
+cd $fold #lvee-engine
 git checkout staging
 rvm install $(cat .ruby-version) #может быть долго
 rvm use $(cat .ruby-version)
 gem install bundler
 bundle install
-sed -i "/config.i18n.backend = I18nDatabaseBackend.new/i \  config.action_mailer.raise_delivery_errors = false\n  config.action_mailer.delivery_method = :smtp\n  config.action_mailer.smtp_settings = {\n    :address => 'smtp.gmail.com',\n    :port => '587',\n    :user_name => '$email',\n    :password => '$email_pass',\n    :authentication => 'plain',\n    :enable_starttls_auto => true }\n" ./config/environments/development.rb
-sed -i "s/P@ssw0rd/$parol/g" config/database.yml
+sed -i "/config.i18n.backend = I18nDatabaseBackend.new/i \  config.action_mailer.raise_delivery_errors = false\n  config.action_mailer.delivery_method = :smtp\n  config.action_mailer.smtp_settings = {\n    :address => 'smtp.gmail.com',\n    :port => '587',\n    :user_name => '$email',\n    :password => '$email_pass',\n    :authentication => 'plain',\n    :enable_starttls_auto => true }\n" $fold/config/environments/development.rb
+sed -i "s/P@ssw0rd/$parol/g" $fold/config/database.yml
 sudo service mysql start # sudo service mariadb start
+cd $fold
+RAILS_ENV=development
 branch=$(git rev-parse --abbrev-ref HEAD)
 if [[ $branch = "staging" ]]; then bundle exec rails bootstrap; fi
 if [[ $branch = "master" ]]; then bundle exec rake bootstrap; fi
-sed -i "s/lvee.org/localhost:3000/g" config/initializers/constants.rb
-sed -i "s/https/http/g" config/initializers/constants.rb
+sed -i "s/lvee.org/localhost:3000/g" $fold/config/initializers/constants.rb
+sed -i "s/https/http/g" $fold/config/initializers/constants.rb
 bundle exec rails s
 bundle exec rake db:drop:all
 
